112154f9de3de470e77baf1e36f6b308d6ab770e
elizabeth <ellizzabbetth@gmail.com>
2023-06-15 18:19:41 -0400

    scraping linkedin (gist)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Ice Breaker
+
+Installing Python 3.11
+
+pipenv:
+https://gist.github.com/monkut/35c2ef098b871144b49f3f9979032cee
---
+++ third_parties/linkedin.py
@@ -0,0 +1,30 @@
+import os
+import requests
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str):
+    """scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinkedIn profile"""
+    #api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    #header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+    print('hi')
+    # response = requests.get(
+    #     api_endpoint, params={"url": linkedin_profile_url}, headers=header_dic
+    # )
+    response = requests.get("https://gist.githubusercontent.com/ellizzabbetth/6f6d0bb78d10ce802be93da22f81f320/raw/e7a8f70e98f036b923979e5e60e67853fcb17f2b/proxycurl.json")
+    print(response)
+
+ 
+    data = response.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    print(data)
+    return data
