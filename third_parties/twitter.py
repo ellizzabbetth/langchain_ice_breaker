5b2d543d5d958bd1f550b50b3e9a720d1e9b678c
elizabeth <ellizzabbetth@gmail.com>
2023-06-16 19:16:38 -0400

    feat: output parser

--- third_parties/twitter.py
+++ third_parties/twitter.py
@@ -2,7 +2,7 @@ import os
 import logging
 
 import tweepy
-
+import requests
 logger = logging.getLogger("twitter")
 
 twitter_client = tweepy.Client(
@@ -32,13 +32,17 @@ def scrape_user_tweets(username, num_tweets=5):
     """
 
     tweets = api.user_timeline(screen_name=username, count=num_tweets)
+    response = requests.get(
+        "https://gist.github.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/eden-marco.json"
+    )
+    data = response.json()
     # user_id = twitter_client.get_user(username=username).data.id
     # tweets = twitter_client.get_users_tweets(
     #     id=user_id, max_results=num_tweets, exclude=["retweets", "replies"]
     # )
 
     tweet_list = []
-    for tweet in tweets.data:
+    for tweet in data.data:
         tweet_dict = {}
         tweet_dict["text"] = tweet["text"]
         tweet_dict["url"] = f"https://twitter.com/{username}/status/{tweet.id}"
@@ -47,5 +51,5 @@ def scrape_user_tweets(username, num_tweets=5):
     return tweet_list
 
 
-if __name__ == "__main__":
-    print(scrape_user_tweets(username="hwchase17"))
+# if __name__ == "__main__":
+#     print(scrape_user_tweets(username="hwchase17"))
